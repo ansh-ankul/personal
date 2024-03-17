235884ec29e2f28a64fc0b70db96dab488ba2209
ansh-ankul <anshankul@gmail.com>
2024-03-17 03:17:32 -0500

    Navbar update

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -22,6 +22,10 @@ const NAV_ITEMS: Array<NavItem> = [
     page: "about",
   },
   {
+    label: "Education",
+    page: "education",
+  },
+  {
     label: "Work Experience",
     page: "work",
   },
